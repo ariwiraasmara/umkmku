7fe84de1439d23e2026659f81ceba8eb58ad19ab
ariwiraasmara.sc37 <ariwiraasmara.sc37@gmail.com>
2024-09-23 09:48:58 +0000

    update

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
 
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
 
 Route::get('/hello', function () {
     return 'hello';
+});
+
+Route::get('/hai', function () {
+    return 'hai';
 });
 
 require __DIR__.'/auth.php';
